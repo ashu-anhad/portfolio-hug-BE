e57d1841914f9087b86de65ee2c648a9984c8990
PRADHAN Ashutosh <ashutosh.pradhan@soprasteria.com>
2024-10-08 13:21:15 +0530

    Initial commit.

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Define Routes
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
